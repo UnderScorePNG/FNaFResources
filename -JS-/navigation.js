32782502b46e87bf9864f3a886eeff08cf2b8180
UnderScorePNG <>
2022-01-19 22:04:13 +0000

    JS Crisis Fix

---
+++ -JS-/navigation.js
@@ -0,0 +1,100 @@
+function closeLeftNav() {
+  document.getElementById("sideNav1").style.width = "0px";
+  document.getElementById("sideNav2").style.width = "0px";
+  document.getElementById("sideNav3").style.width = "0px";
+  document.getElementById("sideNav4").style.width = "0px";
+  document.getElementById("sideNav5").style.width = "0px";
+  document.getElementById("sideNav6").style.width = "0px";
+  document.getElementById("sideNav7").style.width = "0px";
+  document.getElementById("sideNav8").style.width = "0px";
+}
+
+/*-----------------------------------------------------------------*/
+
+function openNav1() {
+  document.getElementById("sideNav1").style.width = "375px";
+  document.getElementById("sideNav2").style.width = "0px";
+  document.getElementById("sideNav3").style.width = "0px";
+  document.getElementById("sideNav4").style.width = "0px";
+  document.getElementById("sideNav5").style.width = "0px";
+  document.getElementById("sideNav6").style.width = "0px";
+  document.getElementById("sideNav7").style.width = "0px";
+  document.getElementById("sideNav8").style.width = "0px";
+}
+
+function openNav2() {
+  document.getElementById("sideNav1").style.width = "0px";
+  document.getElementById("sideNav2").style.width = "375px";
+  document.getElementById("sideNav3").style.width = "0px";
+  document.getElementById("sideNav4").style.width = "0px";
+  document.getElementById("sideNav5").style.width = "0px";
+  document.getElementById("sideNav6").style.width = "0px";
+  document.getElementById("sideNav7").style.width = "0px";
+  document.getElementById("sideNav8").style.width = "0px";
+}
+
+function openNav3() {
+  document.getElementById("sideNav1").style.width = "0px";
+  document.getElementById("sideNav2").style.width = "0px";
+  document.getElementById("sideNav3").style.width = "375px";
+  document.getElementById("sideNav4").style.width = "0px";
+  document.getElementById("sideNav5").style.width = "0px";
+  document.getElementById("sideNav6").style.width = "0px";
+  document.getElementById("sideNav7").style.width = "0px";
+  document.getElementById("sideNav8").style.width = "0px";
+}
+
+function openNav4() {
+  document.getElementById("sideNav1").style.width = "0px";
+  document.getElementById("sideNav2").style.width = "0px";
+  document.getElementById("sideNav3").style.width = "0px";
+  document.getElementById("sideNav4").style.width = "375px";
+  document.getElementById("sideNav5").style.width = "0px";
+  document.getElementById("sideNav6").style.width = "0px";
+  document.getElementById("sideNav7").style.width = "0px";
+  document.getElementById("sideNav8").style.width = "0px";
+}
+
+function openNav5() {
+  document.getElementById("sideNav1").style.width = "0px";
+  document.getElementById("sideNav2").style.width = "0px";
+  document.getElementById("sideNav3").style.width = "0px";
+  document.getElementById("sideNav4").style.width = "0px";
+  document.getElementById("sideNav5").style.width = "375px";
+  document.getElementById("sideNav6").style.width = "0px";
+  document.getElementById("sideNav7").style.width = "0px";
+  document.getElementById("sideNav8").style.width = "0px";
+}
+
+function openNav6() {
+  document.getElementById("sideNav1").style.width = "0px";
+  document.getElementById("sideNav2").style.width = "0px";
+  document.getElementById("sideNav3").style.width = "0px";
+  document.getElementById("sideNav4").style.width = "0px";
+  document.getElementById("sideNav5").style.width = "0px";
+  document.getElementById("sideNav6").style.width = "375px";
+  document.getElementById("sideNav7").style.width = "0px";
+  document.getElementById("sideNav8").style.width = "0px";
+}
+
+function openNav7() {
+  document.getElementById("sideNav1").style.width = "0px";
+  document.getElementById("sideNav2").style.width = "0px";
+  document.getElementById("sideNav3").style.width = "0px";
+  document.getElementById("sideNav4").style.width = "0px";
+  document.getElementById("sideNav5").style.width = "0px";
+  document.getElementById("sideNav6").style.width = "0px";
+  document.getElementById("sideNav7").style.width = "375px";
+  document.getElementById("sideNav8").style.width = "0px";
+}
+
+function openNav8() {
+  document.getElementById("sideNav1").style.width = "0px";
+  document.getElementById("sideNav2").style.width = "0px";
+  document.getElementById("sideNav3").style.width = "0px";
+  document.getElementById("sideNav4").style.width = "0px";
+  document.getElementById("sideNav5").style.width = "0px";
+  document.getElementById("sideNav6").style.width = "0px";
+  document.getElementById("sideNav7").style.width = "0px";
+  document.getElementById("sideNav8").style.width = "375px";
+}
